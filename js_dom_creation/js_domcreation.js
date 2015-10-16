db7734c5660b3e5731a18a5424bc22d98f9ec80f
mihirshahgithub <mshah1086@gmail.com>
2015-10-16 15:31:25 -0700

    js_domcreation.js - Mihir Shah

---
+++ js_dom_creation/js_domcreation.js
@@ -0,0 +1,28 @@
+/* Playground 1 */
+var div_playground1=$("<div>").addClass("playground_source");
+        var h1_playground1=$("<h1>");
+        var p_playground1=$("<p>")
+
+
+    /* Playground 2 */
+
+
+var div_playground2=$("<div>").addClass("playground_source");
+var div_awesome_class=$("<div>").addClass("awesome").text("Here is some awesome div text");
+var div_awesome_class_my_awesome_div2=$("<div>").addClass("awesome_class").attr("id","my_awesome_div_2").text("Here is <strong>EVEN MORE</strong> awesome div text");
+var div_awesome_class_my_awesome_div3=$("<div>").addClass("awesome_class").attr("id","my_awesome_div_3").attr("data-coolness","high").text("Here is (strong>EVEN MORE</strong> awesome div text");
+
+/* Playground 3 */
+<div class="playground playground_source">
+    <ul class="neato">
+    <li>Link 1</li>
+<li>Link 2</li>
+<li>Link 3</li>
+</ul>
+<figure class="awesome_picture">
+    <img src="http://comerecommended.com/wp-content/uploads/2013/01/how-to-get-awesome-back.jpg">
+    <figcaption>That's right, <span class="bold_text">you are</span></figcaption>
+</figure>
+</div>
+
+    var div_playground3=$("<div>").addClass("playground_source")
