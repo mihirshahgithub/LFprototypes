681b0df40064dbe233ffddad3a40df82fdb75dab
mihirshahgithub <mshah1086@gmail.com>
2015-11-24 07:18:04 -0800

    need to configure php interpeter

---
+++ php_includes/index.php
@@ -0,0 +1,12 @@
+<?php
+  $a=1; $b=5;
+  include('includes/data.php');
+  $result = $a * $b;
+  print("<br>Result is $result");
+?>
+/**
+ * Created by PhpStorm.
+ * User: mihirshah
+ * Date: 11/23/15
+ * Time: 5:14 PM
+ */
