7d76b26a59f40bbdce2e077aead93113a1d23aef
mihirshahgithub <mshah1086@gmail.com>
2015-11-24 17:15:35 -0800

    working on superglobals prototype, just starting up

---
+++ php_superglobals/index.php
@@ -0,0 +1,21 @@
+<?php
+print_r($_GET);
+var_dump($_POST);
+?>
+
+Response: Array
+(
+[dog] => pitbull
+[lizards] => iguana
+)
+array(4) {
+["name"]=>
+string(5) "mihir"
+["course"]=>
+string(2) "lf"
+["grade"]=>
+string(2) "60"
+["api_key"]=>
+string(10) "7cdgnHXVY4"
+}
+
