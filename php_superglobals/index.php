f5fa5a79fbd6164922fb322a0ffeaf36b2cdb9fc
mihirshahgithub <mshah1086@gmail.com>
2015-11-25 08:24:56 -0800

    PHP SUPERGLOBAL PROTOTYPE COMPLETE

--- php_superglobals/index.php
+++ php_superglobals/index.php
@@ -3,19 +3,6 @@ print_r($_GET);
 var_dump($_POST);
 ?>
 
-Response: Array
-(
-[dog] => pitbull
-[lizards] => iguana
-)
-array(4) {
-["name"]=>
-string(5) "mihir"
-["course"]=>
-string(2) "lf"
-["grade"]=>
-string(2) "60"
-["api_key"]=>
-string(10) "7cdgnHXVY4"
-}
-
+/*
+PostMan Post and Get both deliever the response when index.php is run in the browser.
+*/
