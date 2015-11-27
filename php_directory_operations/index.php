03e84563b5c6a411b778bd0c3306021ce7c9f6e7
mihirshahgithub <mshah1086@gmail.com>
2015-11-27 12:48:36 -0800

    working on finishing feature set 1 for prototype

--- php_directory_operations/index.php
+++ php_directory_operations/index.php
@@ -1,45 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+</head>
+<body>
 <div id="image_container"></div>
 <?php
-$images=(glob("images/*"));
+$images = (glob("images/*"));
 foreach ($images as $image) {
-    echo "<img src=".$image." />";
-};?>
+    echo "<img src=" . $image . " />";
+}; ?>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+   <ol>
+    <?php
+for($i=0;$i<count($images);$i++){
+    //    print_r('<li data-target="#myCarousel" data-slide-to=' . "$i" . 'class="active">');
+//    print_r('<li data-target="#myCarousel" data-slide-to=' . "$i" . '>');
+    if($i===0){
+        print('<li data-target="#myCarousel" data-slide-to=' . "$i" . 'class="active">');
+    }else{
+        print('<li data-target="#myCarousel" data-slide-to=' . "$i" . '>');
+    }
 
-div id="myCarousel" class="carousel slide" data-ride="carousel">
-<!-- Indicators -->
-<ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
-    <li data-target="#myCarousel" data-slide-to="3"></li>
-</ol>
 
-<!-- Wrapper for slides -->
-<div class="carousel-inner" role="listbox">
-    <div class="item active">
-        <img src="img_chania.jpg" alt="Chania">
-    </div>
 
-    <div class="item">
-        <img src="img_chania2.jpg" alt="Chania">
-    </div>
+}
+    ?>
+       </ol>
 
-    <div class="item">
-        <img src="img_flower.jpg" alt="Flower">
-    </div>
 
-    <div class="item">
-        <img src="img_flower2.jpg" alt="Flower">
-    </div>
-</div>
 
-<!-- Left and right controls -->
-<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-</a>
-<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-</a>
-</div>
+</body>
+</html>
