52277b365fb8c671eab1fd4dc9b147878a7b5891
mihirshahgithub <mshah1086@gmail.com>
2015-12-02 07:58:56 -0800

    working on prototype 2

--- php-sessions/session_reader.php
+++ php-sessions/session_reader.php
@@ -1,1 +1,7 @@
-<!-- Session Reader -->
+<?php
+header('location: session_setter.php');
+$errors=$_SESSION['errors'];
+print_r($_POST);
+$_SESSION['value']=$_POST;
+print_r($_SESSION);
+?>
--- php-sessions/session_setter.php
+++ php-sessions/session_setter.php
@@ -1,1 +1,9 @@
-<!-- Session Setter -->
+<?php
+session_start();
+?>
+<form action="session_reader.php" method="POST">
+    <input type="text" name="name" placeholder="Name" value="<?php print($_SESSION['value']['name'])?>"><br>
+    <input type="text" name="age" placeholder="Age" value="<?php print($_SESSION['value']['age'])?>"><br>
+    <input type="text" name="occupation" placeholder="Occupation" value="<?php print($_SESSION['value']['occupation'])?>"><br>
+    <button type="submit" value="button">Submit</button>
+</form><!-- Session Setter -->
