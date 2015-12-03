79af21e1f8475a8f54fbd3152111c14e2c0d274c
mihirshahgithub <mshah1086@gmail.com>
2015-12-02 17:50:51 -0800

    PHP SESSIONS PROTOTYPE

--- php-sessions/session_reader.php
+++ php-sessions/session_reader.php
@@ -1,7 +1,36 @@
 <?php
-header('location: session_setter.php');
-$errors=$_SESSION['errors'];
-print_r($_POST);
+session_start();
+$_SESSION['errors']=[];
+
+//print_r($_POST);
 $_SESSION['value']=$_POST;
-print_r($_SESSION);
+//print_r($_SESSION);
+
+$formName='/^.{2,}$/';
+$formAge='/^\d+$/';
+$formOccupation='/^.\w+$/';
+
+$_SESSION['name'] = $_POST['name'];
+if(preg_match($formName,$_POST['name'])){
+
+}
+else{
+    $_SESSION['errors']['name']="Name Must be More than One Character";
+}
+$_SESSION['age'] = $_POST['age'];
+if(preg_match($formAge,$_POST['age'])){
+
+}
+else{
+    $_SESSION['errors']['age']="Age must be a number";
+}
+$_SESSION['occupation'] = $_POST['occupation'];
+if(preg_match($formOccupation,$_POST['occupation'])){
+
+}
+else{
+    $_SESSION['errors']['occupation']="Name needs to be more than 1 character";
+}
+header('location: session_setter.php');
 ?>
+
--- php-sessions/session_setter.php
+++ php-sessions/session_setter.php
@@ -1,9 +1,23 @@
 <?php
 session_start();
+//print_r($_SESSION);
+if(empty($_SESSION)){
+    $_SESSION['name']='';
+    $_SESSION['age']='';
+    $_SESSION['occupation']='';
+}
 ?>
 <form action="session_reader.php" method="POST">
-    <input type="text" name="name" placeholder="Name" value="<?php print($_SESSION['value']['name'])?>"><br>
-    <input type="text" name="age" placeholder="Age" value="<?php print($_SESSION['value']['age'])?>"><br>
-    <input type="text" name="occupation" placeholder="Occupation" value="<?php print($_SESSION['value']['occupation'])?>"><br>
+    <input type="text" name="name" placeholder="Name" value="<?php if(isset($_SESSION['name'])) print($_SESSION['name']); ?>">
+    <input type="text" name="age" placeholder="Age" value="<?php if(isset($_SESSION['age'])) print($_SESSION['age']);?>">
+    <input type="text" name="occupation" placeholder="Occupation" value="<?php if(isset($_SESSION['occupation'])) print($_SESSION['occupation']);?>">
     <button type="submit" value="button">Submit</button>
+<?php
+if(isset($_SESSION['errors'])) {
+    foreach ($_SESSION['errors'] as $values) {
+        print("<p>{$values}</p>");
+    }
+}
+
+?>
 </form><!-- Session Setter -->
