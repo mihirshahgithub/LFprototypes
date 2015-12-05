6620475a6f651ae71d3e243e78e8f11aac21ccc8
mihirshahgithub <mshah1086@gmail.com>
2015-12-04 18:06:07 -0800

    feature set 4 done

--- php-mysql/index_insert.php
+++ php-mysql/index_insert.php
@@ -1,1 +1,5 @@
 <!-- index_insert.php -->
+<?php
+require('mysql_connect.php');
+$query="INSERT INTO `todo_items(FS2)`(`ID`, `TITLE`, `DETAILS`, `TIMESTAMP`, `USER_ID`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5])"
+?>
--- php-mysql/index_select.php
+++ php-mysql/index_select.php
@@ -1,1 +1,14 @@
 <!-- index_select.php -->
+<?php
+require('mysql_connect.php');
+ $query="SELECT * FROM `todo_items(FS2)` WHERE `user_id`='3'";
+
+$result=mysqli_query($conn, $query);
+//print_r($result);
+if(mysqli_num_rows($result)>0){
+    while($row=mysqli_fetch_assoc($result)){
+        $output[]=$row;
+    }
+    print_r($output)
+}
+
