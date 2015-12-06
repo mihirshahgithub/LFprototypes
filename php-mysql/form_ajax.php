3dbcefde060de34a4b27b6082e0f3d5bc385b0d0
mihirshahgithub <mshah1086@gmail.com>
2015-12-06 15:29:55 -0800

    php mysql prototype completed

---
+++ php-mysql/form_ajax.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+<head>
+    <script src="https://code.jquery.com/jquery-2.1.4.js"></script>
+    <script>
+        //declare variables in JS
+        var id;
+        var title;
+        var details;
+        function ajax() {
+            $.ajax({
+                method: 'POST',
+                datatype: 'JSON',
+                data: {
+                    id: id.val(),
+                    title: title.val(),
+                    details: details.val()
+                },
+                url: 'index_insert.php',
+                success: function (result) {
+                    console.log('AJAX CALLED WITH', result);
+                    document.write(result);
+                }
+            });
+
+        }
+
+        $(document).ready(function () {
+            title = $('.title');
+            details = $('.details');
+            id = $('.id');
+            $('button').click(ajax);
+        })
+
+    </script>
+</head>
+<body>
+<form>
+    <input type="text" name="title" placeholder="Title" class="title">
+    <br>
+    <input type="text" name="details" placeholder="Details" class="details">
+    <br>
+    <input type="text" name="id" placeholder="ID" class="id">
+    <br>
+    <button type="submit">Submit</button>
+    <!--    <input type="submit" value="SEND">-->
+</form>
+</body>
+</html>
--- php-mysql/index_insert.php
+++ php-mysql/index_insert.php
@@ -1,26 +1,27 @@
 <!-- index_insert.php -->
+
+<?php
+require('mysql_connect.php');
+//$query="INSERT INTO `todo_items(FS2)`(`ID`, `TITLE`, `DETAILS`, `TIMESTAMP`, `USER_ID`) VALUES ('{$id}','{$title}','{$details}',NOW())";
+//print_r($query);
+//mysqli_query($conn,$query);
+?>
 <?php
 
 if(!empty($_POST)){
     $id=$_POST['id'];
     $title=$_POST['title'];
     $details=$_POST['details'];
-    $query="INSERT INTO `todo_items(FS2)`(`ID`, `TITLE`, `DETAILS`,) VALUES ('{$id}','{$title}','{$details})";
-
+    $query="INSERT INTO `todo_items(FS2)`(`ID`, `TITLE`, `DETAILS`,) VALUES ('{$id}','{$title}','{$details}')";
+    mysqli_query($conn,$query);
 }
 ?>
 
-<?php
-require('mysql_connect.php');
-//$query="INSERT INTO `todo_items(FS2)`(`ID`, `TITLE`, `DETAILS`, `TIMESTAMP`, `USER_ID`) VALUES ('{$id}','{$title}','{$details}',NOW())";
-//print_r($query);
-mysqli_query($conn,$query);
-?>
 
 <?php
 if(mysqli_affected_rows($conn)>0){
     $output="I'm the shit";
-//    print_r($output);
+    print_r($output);
 }
 
 print_r($_POST);
